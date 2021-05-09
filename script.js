7de9acbda4fe90aa73e0c1b28881b993e2043aed
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-05-10 01:57:25 +0700

    add ft: cal CPP type 2 expected value + variance

--- script.js
+++ script.js
@@ -13,74 +13,88 @@ const cpSubmitOnClick = () => {
     var type_1 = document.getElementById("type_1").checked;
     var type_2 = document.getElementById("type_2").checked;
     var p_lst = document.getElementById("cp_prob").value
-                                .split("\n").filter(p => !isNaN(p))
-                                .map(p => parseInt(p));
+                        .split("\n")
+                        .map(p => parseFloat(p))
+                        .filter(p => !isNaN(p) && p > 0 && p <= 1);
     console.log(p_lst);
     if (!validateInput(n, type_1, type_2, p_lst)) return;
     if (type_1) cal_type_1(n);
         
-    else cal_type_2 (n, p_lst);
+    else cal_type_2 (p_lst);
   
 }
 
 function validateInput (n, type_1, type_2, p_lst) {
-    var err = document.getElementById("cp_exception").innerHTML;
-    if (n == "") err += "Error: number of coupon types can't be empty <br />";
+    if (n == "") 
+        document.getElementById("cp_exception").innerHTML += "Error: number of coupon types can't be empty <br />";
 
-    if (!(type_1 | type_2)) err += "Error: problem type unchecked <br />";
+    if (!(type_1 | type_2)) 
+        document.getElementById("cp_exception").innerHTML += "Error: problem type unchecked <br />";
     
-    if (type_2 && p_lst.length != n) err += 
-        "Error: missing/ incorrect-format coupon probability input <br />";
+    if (type_2 && p_lst.length != n) 
+        document.getElementById("cp_exception").innerHTML += "Error: missing/ incorrect-format coupon probability input <br />";
     
-    if (type_2 && p_lst.length == n && p_lst.reduce((a, b) => a + b, 0) != 1)
-        err += "Error: sum of coupon probability is not 1 <br />";
+    if (type_2 && p_lst.reduce((a, b) => a + b, 0) != 1)
+        document.getElementById("cp_exception").innerHTML += "Error: sum of coupon probability is not 1 <br />";
 
-    return  n != "" && (type_1 | type_2 && p_lst.length == n);
+    return  n != "" && (type_1 | (type_2 && p_lst.length == n
+                                    && p_lst.reduce((a, b) => a + b, 0) == 1));
 }
 
 function cal_type_1 (n) {
-    document.getElementById("cp_expectedV").innerHTML = epv_type_1(n);
-    document.getElementById("cp_var").innerHTML = var_type_1(n);
-}
-
-
-const epv_type_1 = (n) => {
-    return n * harmonic (n);
-}
-
-const harmonic = (n) => {
-    var sum = 0;
-    for (var i = 1; i <= n; i ++) {
-        sum += 1/i;
-    }
-    return sum;
-}
-
-const var_type_1 = (n) => {
     var sum1 = 0;
     var sum2 = 0;
     for (var i = 1; i <= n; i ++) {
-        sum1 += 1/ Math.pow(i, 2);
-        sum2 += 1/i;
+        sum1 += 1/i;
+        sum2 += 1/ Math.pow(i, 2);
     }
-
-    return Math.pow(n, 2) * sum1 - n * sum2;
-
-   
-
+    var exp_v = sum1 * n;
+    var variance = Math.pow(n, 2) * sum2 - n * sum1;
+    document.getElementById("cp_expectedV").innerHTML = exp_v;
+    document.getElementById("cp_var").innerHTML = variance;
 }
 
-function cal_type_2 (n, p_lst) {
-    combine_sum_p(p_lst);
+
+
+function cal_type_2 (p_lst) {
+    // get sum of p_combinations table
+    var sum_p_table = combine_sum_p(p_lst);
+    console.log(sum_p_table);
+
+    // expected value, variance
+    var exp_v = 0;
+    var variance = 0; 
+    
+    for (const opr_count of sum_p_table.keys()) {
+        sums = sum_p_table.get(opr_count);
+        sums.forEach(p_sum => {
+            exp_v += Math.pow (-1, opr_count-1) * (1/p_sum);
+                    
+            variance += Math.pow (-1, opr_count-1) * (1/ Math.pow(p_sum, 2));
+            console.log(exp_v);
+            console.log("var");
+            console.log(variance);
+            return exp_v, variance;
+        });
+      
+    }
+    variance = variance - Math.pow(exp_v, 2) - exp_v; 
+
+    // show result
+    document.getElementById("cp_expectedV").innerHTML = exp_v;
+    document.getElementById("cp_var").innerHTML = variance;
+    
 }
+
+
 
 function combine_sum_p (p_lst) {
-    let table =  new Map(); // key: length of combo, value: sum 
+    let table =  new Map(); // key: number of operands, value: sum of combin. of operands 
     for (var i = 1; i <= p_lst.length; i++){
         table.set(i, []);
     }
     combine(p_lst, table, -1, 0, 0);
-    console.log(table);
+    return table;
 
 
 }
