8f21e8d1118cb0f1ecea9e80377116262e1ecece
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-05-09 19:00:13 +0700

    add ft: calculate CCP type 1 expected value, var

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-const submitOnClick = () => {
-    const input = document.getElementById("coupon_input").value;
-    console.log(input);
-    document.getElementById("coupon_ret").innerHTML = harmonic(input)*input;
+const cpType1OnClick = () => {
+    const n = document.getElementById("cp_count").value;
+    if (n != "") {
+        document.getElementById("cp_prob").value = 1/n;
+    } else {
+        document.getElementById("cp_prob").value = "";
+    }
+  
+   
+}
+const cpSubmitOnClick = () => { 
+    if (!validateInput()) return;
+    var n = document.getElementById("cp_count").value;
+    if (document.getElementById("type_1").checked) {
+        document.getElementById("cp_expectedV").innerHTML = e_type_1(n);
+        document.getElementById("cp_var").innerHTML = var_type_1(n);
+        return;
+    }
 
+
+
+}
+
+function validateInput () {
+    var n = document.getElementById("cp_count").value;
+    var type_1 = document.getElementById("type_1").checked;
+    var type_2 = document.getElementById("type_2").checked;
+    if (n == "") {
+        document.getElementById("cp_exception").innerHTML += 
+        "number of coupon types can't be empty <br />";
+        
+    }
+
+    if (!(type_1 | type_2)) {
+        document.getElementById("cp_exception").innerHTML += 
+        "problem type unchecked <br />";
+    }
+    return  n != "" && (type_1 | type_2);
+}
+
+const e_type_1 = (n) => {
+    return n * harmonic (n);
 }
 
 const harmonic = (n) => {
@@ -12,3 +49,19 @@ const harmonic = (n) => {
     }
     return sum;
 }
+
+const var_type_1 = (n) => {
+    var sum1 = 0;
+    var sum2 = 0;
+    for (var i = 1; i <= n; i ++) {
+        sum1 += 1/ Math.pow(i, 2);
+        sum2 += 1/i;
+    }
+
+    return Math.pow(n, 2) * sum1 - n * sum2;
+
+   
+
+}
+
+
