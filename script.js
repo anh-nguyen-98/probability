a9fe143433eb5736ab1a13eb4792467c291be370
Gabriel Nguyen <huyen.nguyen.190033@student.fulbright.edu.vn>
2021-05-19 18:10:29 +0700

    add css

--- script.js
+++ script.js
@@ -169,7 +169,21 @@ function build_chart(ctx, dist) {
     catch (err) {
         throw ("Error");
     }
-   
+
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("calculator");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace("w3-red", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-red";
+}
+document.getElementById("myLink").click();
 }
 
 
