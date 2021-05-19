d9f14ee5621283dc5f9ac34d32ffc09188307a1b
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-05-19 17:12:20 +0700

    add feature: calculate QTP type 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CPP
 const cpType1OnClick = () => {
     const n = document.getElementById("cp_count").value;
     if (n != "") {
@@ -17,14 +18,14 @@ const cpSubmitOnClick = () => {
                         .map(p => parseFloat(p))
                         .filter(p => !isNaN(p) && p > 0 && p <= 1);
     console.log(p_lst);
-    if (!validateInput(n, type_1, type_2, p_lst)) return;
-    if (type_1) cal_type_1(n);
+    if (!cpValidateInput(n, type_1, type_2, p_lst)) return;
+    if (type_1) cpCalType1(n);
         
-    else cal_type_2 (p_lst);
+    else cpCalType2 (p_lst);
   
 }
 
-function validateInput (n, type_1, type_2, p_lst) {
+function cpValidateInput (n, type_1, type_2, p_lst) {
     if (n == "") 
         document.getElementById("cp_exception").innerHTML += "Error: number of coupon types can't be empty <br />";
 
@@ -41,7 +42,7 @@ function validateInput (n, type_1, type_2, p_lst) {
                                     && p_lst.reduce((a, b) => a + b, 0) == 1));
 }
 
-function cal_type_1 (n) {
+function cpCalType1 (n) {
     var sum1 = 0;
     var sum2 = 0;
     for (var i = 1; i <= n; i ++) {
@@ -71,13 +72,13 @@ function cal_type_1 (n) {
     document.getElementById("cp_expectedV").innerHTML = exp_v;
     document.getElementById("cp_var").innerHTML = variance;
     var ctx = document.getElementById("cp_dist").getContext('2d');
-    build_chart(ctx, dist);
+    buildChart(ctx, dist);
       
 }
 
 
 
-function cal_type_2 (p_lst) {
+function cpCalType2 (p_lst) {
     // get sum of p_combinations table
     var sum_p_table = combine_sum_p(p_lst);
     // console.log(sum_p_table);
@@ -118,7 +119,7 @@ function cal_type_2 (p_lst) {
     document.getElementById("cp_expectedV").innerHTML = exp_v;
     document.getElementById("cp_var").innerHTML = variance;
     var ctx = document.getElementById("cp_dist").getContext('2d');
-    build_chart(ctx, dist);
+    buildChart(ctx, dist);
     
 }
 
@@ -145,7 +146,7 @@ function combine (p_lst, table, curr_idx, sum, n) {
 
 }
 
-function build_chart(ctx, dist) {
+function buildChart(ctx, dist) {
     try {
         var myChart = new Chart(ctx, {
             type: 'line',
@@ -171,6 +172,55 @@ function build_chart(ctx, dist) {
     }
    
 }
+
+// M/M/1
+
+const qtSubmitOnClick = () => {
+    var mean_arv_time = document.getElementById("mean_arv_time").value;
+    var mean_svc_time = document.getElementById("mean_svc_time").value;
+    
+
+    var type_1 = document.getElementById("qt_type_1").checked;
+    var type_2 = document.getElementById("qt_type_2").checked;
+    console.log(mean_arv_time);
+    if (type_1) qtCalType1(mean_arv_time, mean_svc_time);
+        
+    else if (type_2) qtCalType2();
+    else throw new Error ("select queueing system type");
+}
+
+function qtCalType1 (mean_arv_time, mean_svc_time) {
+    var lambda = 1/mean_arv_time;
+    var mu = 1/mean_svc_time;
+    var var_arv = 1/Math.pow(lambda, 2);
+    var var_svc = 1/Math.pow(mu, 2);
+    var p_svr_busy = lambda/mu;
+    var l_q = Math.pow(p_svr_busy, 2) / (1 - p_svr_busy);
+    var w_q = l_q/lambda;
+    var w = w_q + 1/mu;
+    var l = lambda * w;
+    var p_svr_idle = 1 - p_svr_busy;
+
+    console.log("success");
+    document.getElementById("mean_arv_rate").innerHTML = lambda;
+    document.getElementById("mean_svc_rate").innerHTML = mu;
+    document.getElementById("var_arv").innerHTML = var_arv;
+    document.getElementById("var_svc").innerHTML = var_svc;
+    document.getElementById("p_svr_busy").innerHTML = p_svr_busy;
+    document.getElementById("mean_q_cust").innerHTML = l_q;
+    document.getElementById("mean_q_wait").innerHTML = w_q;
+    document.getElementById("mean_sys_cust").innerHTML = l;
+    document.getElementById("mean_sys_wait").innerHTML = w;
+    document.getElementById("p_svr_idle").innerHTML = p_svr_idle;
+
+}
+function qtCalType2() {
+
+}
+function qtValidateInput(){
+
+}
+
 
 
 
